5f05f97c4a1e18854df9ee842f877d53c7c616df
x-in <xinliang.zhu@qq.com>
2023-05-23 09:45:21 +0800

    feature(): Optimize packaging configuration

--- basic-project/cmd/install_npm.bat
+++
@@ -1,3 +0,0 @@
-@REM 安装npm依赖
-
-start cmd /k"cd server_dist&&node -v&&npm config set registry https://registry.npm.taobao.org/&&npm config get registry&&npm i http-server -g&&npm i&&exit"
--- test.js
+++
@@ -1,6 +0,0 @@
-import {fileURLToPath} from 'node:url';
-import path from 'node:path';
-// console.log(process.env.npm_config_user_agent)
-console.log(import.meta.url);
-console.log(fileURLToPath(import.meta.url));
-console.log(path.dirname(fileURLToPath(import.meta.url)));
